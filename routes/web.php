39093fccf05ee52739213c055f989f45373905a7
Samit Koyom <samitkoyom@gmail.com>
2025-04-03 01:01:24 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('welcome');
+
+Route::get('/confirmpassword', function () {
+    return Inertia::render('Auth/ConfirmPassword');
+})->name('confirmpassword');
+
+Route::get('/verifyemail', function () {
+    return Inertia::render('Auth/VerifyEmail');
+})->name('verifyemail');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
+
+// ทำการรวมเส้นทางที่เกี่ยวข้องกับการลงทะเบียนผู้ใช้ใหม่
+require __DIR__.'/auth.php';
